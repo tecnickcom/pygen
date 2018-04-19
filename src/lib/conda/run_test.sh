9a16c5efd483e54f803f0b46e25ed7e86824e902
nicolaasuni <info@tecnick.com>
2018-04-19 14:40:34 +0100

    Add pycodestyle inout dir

--- src/lib/conda/run_test.sh
+++ src/lib/conda/run_test.sh
@@ -4,7 +4,7 @@ echo "import coverage; coverage.process_startup()" > "${SP_DIR}/subcov.pth"
 mkdir -p "${CONDA_ROOT}/conda-bld/coverage"
 rm -rf "${CONDA_ROOT}/conda-bld/coverage/*"
 cd ../work \
-&& pycodestyle --max-line-length=120 \
+&& pycodestyle --max-line-length=120 ~#PROJECT#~ \
 && ${PYTHON} setup.py test \
 && cp .coverage.* "${CONDA_ROOT}/conda-bld/coverage"
 cd "${CONDA_ROOT}/conda-bld/coverage" && coverage combine && coverage html
