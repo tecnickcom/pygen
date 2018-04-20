84f42d369bcb3910aa6c66d183cae52640f22802
nicolaasuni <info@tecnick.com>
2018-04-20 11:46:26 +0100

    Add example benchmark tests

--- src/lib/tests/test_example.py
+++ src/lib/tests/test_example.py
@@ -10,3 +10,10 @@ class TestExample(TestCase):
     def test_init_class(self):
         ex = Example(num=123)
         self.assertEqual(ex.get_double(), 246)
+
+
+class TestBenchmarkProcess(object):
+
+    def test_benchmark_get_double(self, benchmark):
+        p = Process(123)
+        benchmark(p.get_double)
--- src/srv/tests/test_process.py
+++ src/srv/tests/test_process.py
@@ -11,3 +11,10 @@ class TestProcess(TestCase):
         p = Process(123)
         o = p.get_double()
         self.assertEqual(o["double"], 246)
+
+
+class TestBenchmarkProcess(object):
+
+    def test_benchmark_get_double(self, benchmark):
+        p = Process(123)
+        benchmark(p.get_double)
